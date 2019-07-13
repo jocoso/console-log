6fc0b96888b8cad926910cfb72ee727aaba5541f
joshua <joshua.collado022@gmail.com>
2019-07-13 17:51:30 -0400

    Replaced functional initialization with a new_list() getter

--- main.c
+++ main.c
@@ -1,11 +1,6 @@
 #include <stdio.h>
-#include "modules/argshandler.h"
-#include "modules/tools/hashmap.h"
+#include "modules/tools/arraylist.h"
 
 int main(int argc, char** argv) {
-	Hash_Table *hash = new_hash();
-	hash_insert(hash, "--argument", "rojo");
-	char* rojo = hash_search(hash, "--argument");
-	printf("%s\n", rojo);
-	hash_free(hash);
+	//void* a = CONVERT_TO_VOID_POINT("rojo");
 }
