41adf44d847d08229001f92700b65e26fe9fc035
joshua <joshua.collado022@gmail.com>
2019-07-20 06:41:52 -0400

    Prepared hashmap functions for creation

--- modules/tools/hashmap.c
+++ modules/tools/hashmap.c
@@ -1,6 +1,44 @@
 #include <string.h>
+#include "../utilities/stddefs.h";
 #include "hashmap.h"
 
+
+Hash_Table *new_hash_table() {}
+
+struct Hash_Node *new_hash_node(key, value) 
+	char* key;
+	void* value;
+{return NULL;}
+
+void hash_insert(table, key, value)
+	Hash_Table *table; 
+	char* key; 
+	void* value;
+{}
+
+void *hash_get(table, key) 
+	Hash_Table *table;
+	char* key;
+{return NULL;}
+
+struct Hash_Node *get_hash_node(table, key) 
+	Hash_Table *table; 
+	char* key;
+{return NULL;}
+
+void hash_delete(table, key) 
+	Hash_Table *table;
+	char* key;
+{}
+	
+const unsigned int hash_code(size, str)
+	const unsigned int size; 
+	const char* str;
+{return -1;}
+
+void hash_free(table) 
+	Hash_Table *table;
+{}
 
 const unsigned int hash_code(size, str) 
 	// Arguments
@@ -17,4 +55,3 @@ const unsigned int hash_code(size, str)
 
 	return code%size;
 }
-
--- modules/tools/hashmap.h
+++ modules/tools/hashmap.h
@@ -6,14 +6,22 @@
 	struct Hash_Node {
 		char* key;
 		void* value;
-	} Hash_Node;
+	};
 
-	typedef struct {
+	typedef struct Hash_Table{
 		unsigned int size;
 		unsigned int capacity;
 		struct Hash_Node** buckets;
 	} Hash_Table;
 
+	Hash_Table *new_hash_table();
+	struct Hash_Node *new_hash_node(char* key, void* value);
+	void hash_insert(Hash_Table *table, char* key, void* value);
+	void *hash_get(Hash_Table *table, char* key);
+	struct Hash_Node *get_hash_node(Hash_Table *table, char* key);
+	void hash_delete(Hash_Table *table, char* key);
 	const unsigned int hash_code(const unsigned int size, const char* str);
+	void hash_free(Hash_Table *table);
+	
 
 #endif
