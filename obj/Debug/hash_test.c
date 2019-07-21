04e933c1babd7b2e54e7c3494754ee72dbb86021
joshua <joshua.collado022@gmail.com>
2019-07-21 17:30:10 -0400

    Debug files implementing new Debugging style

---
+++ obj/Debug/hash_test.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "../../modules/utilities/debug.h"
+#include "../../modules/utilities/stddefs.h"
+#include "../../modules/tools/hashmap.h"
+
+// Success
+boolean can_call_new_and_free_without_a_segfault() {
+    Hash_Table *table_test = new_hash_table();
+    hash_free(table_test);
+    return true;
+}
+
+boolean can_call_once_insert_and_get() {
+    Hash_Table *table_test = new_hash_table();
+    hash_insert(table_test, "Josh", 23);
+    int answer = (int*) hash_get(table_test, "Josh");
+    hash_free(table_test);
+    return (answer == 23);
+}
+
+boolean can_call_multiple_insert_and_get() {
+    Hash_Table *table_test = new_hash_table();
+
+    hash_insert(table_test, "Josh", 23);
+    hash_insert(table_test, "Rosemary", 57);
+    hash_insert(table_test, "Manatee", 200);
+
+    int answer1 = (int*) hash_get(table_test, "Josh");
+    int answer2 = (int*) hash_get(table_test, "Rosemary");
+    int answer3 = (int*) hash_get(table_test, "Manatee");
+
+    hash_free(table_test);
+    
+    return ((answer1 == 23) && (answer2 == 57) && (answer3 == 200));
+}
+
+boolean does_is_empty_do_its_job() {
+    Hash_Table *table_test1 = new_hash_table();
+    Hash_Table *table_test2 = NULL;
+    return !is_empty(table_test1) && is_empty(table_test2);
+}
+
+int main() {
+    boolean test1 = can_call_new_and_free_without_a_segfault();
+    boolean test2 = can_call_once_insert_and_get();
+    boolean test3 = can_call_multiple_insert_and_get(); 
+    boolean test4 = does_is_empty_do_its_job();
+
+    PNT_DEBUG("Can call new() and free() functions without a segfault?", test1, true); 
+    PNT_DEBUG("Can call once insert() and get() functions?", test2, true);
+    PNT_DEBUG("Can call multiple insert() and get() functions?", test3, true);
+    PNT_DEBUG("Does is_empty() do its job", test4, 1);
+
+    return 0;
+}
